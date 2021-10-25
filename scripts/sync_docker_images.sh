ddc6c96d56712ca27d0a1ad6bff4822d4016cbd5
pancudaniel7 <pancudaniel7@gmail.com>
2021-10-25 16:20:08 +0300

    Updated sync file for docker images

--- scripts/sync_docker_images.sh
+++ scripts/sync_docker_images.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker pull apachepulsar/pulsar-all:2.6.0
-kind load
+docker pull streamnative/pulsar-operator:v0.8.2 && \
+docker tag streamnative/pulsar-operator:v0.8.2 localhost:5000/streamnative/pulsar-operator:v0.8.2 && \
+kind load docker-image streamnative/pulsar-operator:v0.8.2 --name kind
+
+docker pull gcr.io/google-containers/kube-scheduler:v1.14.10 && \
+docker tag gcr.io/google-containers/kube-scheduler:v1.14.10 localhost:5000/google-containers/kube-scheduler:v1.14.10 && \
+kind load docker-image gcr.io/google-containers/kube-scheduler:v1.14.10 --name kind
